a4097b9092e769642555de70d291157b5cd2cdad
Valentino Abate <valentinoabate1@gmail.com>
2020-11-26 00:47:00 -0800

    Item Randomization Prep
    - Recorded spriteset indices for Fire Red and Emerald
    - Added relevant script commands

--- PokemonRandomizer/PokemonRandomizer/Backend/Scripting/Gen3Opcodes.cs
+++ PokemonRandomizer/PokemonRandomizer/Backend/Scripting/Gen3Opcodes.cs
@@ -10,8 +10,6 @@ namespace PokemonRandomizer.Backend.Scripting
     {
         public const byte setRegister = 0x20;
         public const byte addRegister = 0x30;
-        public const byte reg0 = 0x00, reg1 = 0x01, reg2 = 0x02, 
-            reg3 = 0x03, reg4 = 0x04, reg5 = 0x05, reg6 = 0x06, reg7 = 0x07;
-
+        public const byte reg0 = 0x00, reg1 = 0x01, reg2 = 0x02, reg3 = 0x03, reg4 = 0x04, reg5 = 0x05, reg6 = 0x06, reg7 = 0x07;
     }
 }
