333e19b99f8d7cd8b0f02026bcad2d2470a1c1ec
Valentino Abate <valentinoabate1@gmail.com>
2022-03-03 23:08:28 -0800

    using cleanup

--- PokemonRandomizer/PokemonRandomizer/UI/Models/TrainerOrganizationDataModel.cs
+++ PokemonRandomizer/PokemonRandomizer/UI/Models/TrainerOrganizationDataModel.cs
@@ -1,9 +1,4 @@
 ﻿using PokemonRandomizer.UI.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static PokemonRandomizer.Settings;
 
 namespace PokemonRandomizer.UI.Models
