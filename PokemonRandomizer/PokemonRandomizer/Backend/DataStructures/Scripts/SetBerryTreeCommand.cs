3ae5b1dde4103ac8a8506683a8ae8626c28dc85a
Valentino Abate <valentinoabate1@gmail.com>
2022-08-03 22:30:08 -0700

    Cleanup

--- PokemonRandomizer/PokemonRandomizer/Backend/DataStructures/Scripts/SetBerryTreeCommand.cs
+++ PokemonRandomizer/PokemonRandomizer/Backend/DataStructures/Scripts/SetBerryTreeCommand.cs
@@ -1,10 +1,5 @@
 ﻿using PokemonRandomizer.Backend.EnumTypes;
 using PokemonRandomizer.Backend.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PokemonRandomizer.Backend.DataStructures.Scripts
 {
