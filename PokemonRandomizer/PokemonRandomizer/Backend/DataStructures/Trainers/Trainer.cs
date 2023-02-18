63dc0c624b38f17a217894492fdbd7627d2e0229
Valentino Abate <valentinoabate1@gmail.com>
2023-02-17 18:14:29 -0800

    Remove Winforms Dependency

--- PokemonRandomizer/PokemonRandomizer/Backend/DataStructures/Trainers/Trainer.cs
+++ PokemonRandomizer/PokemonRandomizer/Backend/DataStructures/Trainers/Trainer.cs
@@ -3,9 +3,6 @@ using PokemonRandomizer.Backend.Utilities.Repointing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
 
 namespace PokemonRandomizer.Backend.DataStructures.Trainers
 {
