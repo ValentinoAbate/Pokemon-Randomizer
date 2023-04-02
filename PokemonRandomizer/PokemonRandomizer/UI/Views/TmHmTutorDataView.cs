4c51a2c66ce05794da80b736c16b3bf08ddc65cd
Valentino Abate <valentinoabate1@gmail.com>
2023-04-02 12:18:06 -0700

    TM/HM/Tutor Tooltip Improvements

--- PokemonRandomizer/PokemonRandomizer/UI/Views/TmHmTutorDataView.cs
+++ PokemonRandomizer/PokemonRandomizer/UI/Views/TmHmTutorDataView.cs
@@ -16,9 +16,11 @@ namespace PokemonRandomizer.UI.Views
 
         private const string intelligentRandomTooltip = "The chance that any non-NORMAL move whose compatibility can't be explicitly determined will be compatible";
         private const string intelligentRandomNormalTooltip = "The chance that any NORMAL move whose compatibility can't be explicitly determined will be compatible";
+        private const string compatOptionTooltip = "The strategy used to determing TM/HM/Tutor compatibility" +
+            "\nRead the tooltips of the options in the dropdown menu for more info on how they work";
         private static CompositeCollection CompatOptionDropdown => new()
         {
-            new ComboBoxItem() {Content="Unchanged" },
+            new ComboBoxItem() {Content="Unchanged", ToolTip="Compatiblity will be left as it is in the base game. This may cause compatibility to not make sense if TM or Tutors are randomized and for Variant Pokemon" },
             new ComboBoxItem() {Content="Full Compatibility", ToolTip="All pokemon will be compatible with all TMs, HMs, and Tutors" },
             new ComboBoxItem() {Content="Random", ToolTip="Randomly select TM and Tutor compatibility. Does not randomize HM compatibility" },
             new ComboBoxItem() {Content="Random (Keep Number)", ToolTip="Randomly select TM and Tutor compatibility, but keep the same number the pokemon had in the base ROM. Does not randomize HM compatibility"},
@@ -28,15 +30,15 @@ namespace PokemonRandomizer.UI.Views
         public TmHmTutorDataView(TmHmTutorModel model)
         {
             var stack = CreateMainStack();
-            stack.Header("Randomization");
-            stack.Add(new RandomChanceUI("Random TM Moves", model.RandomizeTMs, model.TMRandChance));
-            stack.Add(new RandomChanceUI("Random Tutor Moves", model.RandomizeMoveTutors, model.MoveTutorRandChance));
-            stack.Add(new BoundCheckBoxUI("Prevent HM moves in TMs and Tutors", model.NoHmMovesInTMsAndTutors));
-            stack.Add(new BoundCheckBoxUI("Prevent duplicate moves in TMs and Tutors", model.NoDuplicateTMsAndTutors));
+            stack.Header("Tm/Tutor Move Randomization");
+            stack.Add(new RandomChanceUI("Random TM Moves", model.RandomizeTMs, model.TMRandChance) { ToolTip = "Randomize moves taught by TMs" });
+            stack.Add(new RandomChanceUI("Random Tutor Moves", model.RandomizeMoveTutors, model.MoveTutorRandChance) { ToolTip = "Randomize moves taught by Move Tutor NPCs" });
+            stack.Add(new BoundCheckBoxUI("Prevent HM moves in TMs and Tutors", model.NoHmMovesInTMsAndTutors, "Prevents HM moves from appearing as TMs or Move Tutors"));
+            stack.Add(new BoundCheckBoxUI("Prevent duplicate moves in TMs and Tutors", model.NoDuplicateTMsAndTutors, "Ensures all TMs and Move Tutors are unique"));
             stack.Add(new BoundCheckBoxUI("Keep important TMs and Tutors", model.KeepImportantTmsAndTutors, "Ensures that important TMs and Tutors won't be randomized (Secret Power + Dig in RSE)"));
             stack.Separator();
-            stack.Header("Compatibility");
-            var compatDropdown = stack.Add(new EnumComboBoxUI<CompatOption>("Compatibility Strategy", CompatOptionDropdown, model.MoveCompatOption));
+            stack.Header("Tm/Hm/Tutor Compatibility");
+            var compatDropdown = stack.Add(new EnumComboBoxUI<CompatOption>("Compatibility Strategy", CompatOptionDropdown, model.MoveCompatOption) { ToolTip = compatOptionTooltip });
             stack.Add(compatDropdown.BindVisibility(new BoundSliderUI("Random Compatibility Chance", model.RandomCompatTrueChance), (int)CompatOption.Random, (int)CompatOption.RandomKeepNumber));
             stack.Add(compatDropdown.BindVisibility(new BoundSliderUI("Intelligent Compatibiliy Random Chance (NORMAL Moves)", model.IntelligentCompatNormalRandChance, true, 0.01, 0, 0.5) { ToolTip = intelligentRandomNormalTooltip}, (int)CompatOption.Intelligent));
             stack.Add(compatDropdown.BindVisibility(new BoundSliderUI("Intelligent Compatibiliy Random Chance (Other)", model.IntelligentCompatNoise, true, 0.01, 0, 0.33) { ToolTip = intelligentRandomTooltip }, (int)CompatOption.Intelligent));
