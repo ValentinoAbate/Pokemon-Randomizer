d618195857f24d0f8956d88858f33fd409a263c1
Valentino Abate <valentinoabate1@gmail.com>
2023-04-07 15:26:17 -0700

    Starter pokemon UI tooltip pass

--- PokemonRandomizer/PokemonRandomizer/UI/Views/StartersDataView.cs
+++ PokemonRandomizer/PokemonRandomizer/UI/Views/StartersDataView.cs
@@ -11,16 +11,16 @@ namespace PokemonRandomizer.UI.Views
     {
         public CompositeCollection StarterOptionDropdown => new CompositeCollection()
         {
-            new ComboBoxItem() {Content="Unchanged" },
-            new ComboBoxItem() {Content="Random" },
-            new ComboBoxItem() {Content="Random Type Triangle", ToolTip="Randomly select three starters where each is strong against the next"},
+            new ComboBoxItem() {Content="Unchanged", ToolTip="Leave the starter pokemon as they are in the base game" },
+            new ComboBoxItem() {Content="Random", ToolTip="Randomly select three unevolved pokemon as starters" },
+            new ComboBoxItem() {Content="Random Type Triangle", ToolTip="Randomly select three unevolved pokemon that form a triangle where each is strong against the next as starters"},
             new ComboBoxItem() {Content="Custom", ToolTip="Set 1 or more custom starters"},
         };
         private const string strongTriTooltip = "Only generate type triangles where each pokemon is super effective against AND resistant to the next (as opposed to just super effective against)";
         private const string safeMovesetsTooltip = "Ensure that starters will have attacking move(s) at level 5 that can hit all pokemon in the game" +
             "\nAny starter whose moveset is unsafe will be given Foresight or Odor Sleuth, and Tackle / Astonish if necessary" +
             "\nAny moves that would be skipped over at level 5 due to added moves will instead be learned at level 6";
-        private const string banLegendariesTooltip = "Ban legendaries from being chosen as random starters. Legendaries specifically selected as Custom starters will not be affected";
+        private const string banLegendariesTooltip = "Ban legendaries from being chosen as random starters. Legendaries specifically selected as custom starters will not be affected";
         public StartersDataView(StartersDataModel model, string[] pokemonNames, List<Pokemon> pokemon)
         {
             // Create stack and add content
@@ -28,7 +28,7 @@ namespace PokemonRandomizer.UI.Views
             stack.Header("Randomization");
 
             // Randomization Strategy CB
-            var optionCb = stack.Add(new EnumComboBoxUI<StarterPokemonOption>("Randomization Strategy", StarterOptionDropdown, model.StarterSetting));
+            var optionCb = stack.Add(new EnumComboBoxUI<StarterPokemonOption>("Randomization Strategy", StarterOptionDropdown, model.StarterSetting) { ToolTip="The strategy used to select starter pokemon" });
             // Type Triangle UI
             optionCb.BindVisibility(stack.Add(new BoundCheckBoxUI("Force Strong Type Triangle", model.StrongStarterTypeTriangle, strongTriTooltip)), (int)StarterPokemonOption.RandomTypeTriangle);
 
@@ -40,7 +40,7 @@ namespace PokemonRandomizer.UI.Views
                 return new BoundComboBoxUI("", pokemonOptions, pokemon.IndexOf(model.CustomStarters[index]), i => model.CustomStarters[index] = pokemon[i]);
             }
             var customStarterStack = stack.Add(CreateHorizontalStack());
-            customStarterStack.Add(new Label() { Content = "Custom Starters:" }, CustomStarterCB(0), CustomStarterCB(1), CustomStarterCB(2));
+            customStarterStack.Add(new Label() { Content = "Custom Starters:", ToolTip="The custom starter pokemon to use (any pokemon set to \"Random\" will be a random unevolved pokemon)" }, CustomStarterCB(0), CustomStarterCB(1), CustomStarterCB(2));
             optionCb.BindVisibility(customStarterStack, (int)StarterPokemonOption.Custom);
 
             // Additional Settings
