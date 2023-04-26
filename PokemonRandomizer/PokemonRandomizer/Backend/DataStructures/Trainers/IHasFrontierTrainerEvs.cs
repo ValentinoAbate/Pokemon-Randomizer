e04aa375eb83a13e9d8d4046e44166f2ed11ee3c
Valentino Abate <valentinoabate1@gmail.com>
2023-04-25 22:45:47 -0700

    Fill out frontier trainer EV values

--- PokemonRandomizer/PokemonRandomizer/Backend/DataStructures/Trainers/IHasFrontierTrainerEvs.cs
+++ PokemonRandomizer/PokemonRandomizer/Backend/DataStructures/Trainers/IHasFrontierTrainerEvs.cs
@@ -11,7 +11,13 @@ namespace PokemonRandomizer.Backend.DataStructures.Trainers
         [System.Flags]
         public enum EvFlags
         {
-
+            None = 0,
+            HP = 1,
+            Atk = 2,
+            Def = 4,
+            Speed = 8,
+            SpAtk = 16,
+            SpDef = 32,
         }
 
         public EvFlags Evs { get; set; }
