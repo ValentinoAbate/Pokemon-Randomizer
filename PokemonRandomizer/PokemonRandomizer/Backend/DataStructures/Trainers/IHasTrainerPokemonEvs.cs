0143d0586b7f0a62a4465ea548a41bdae18ac890
Valentino Abate <valentinoabate1@gmail.com>
2023-08-13 16:33:21 -0700

    Add EV helpers
    - Named EV properties
    - constants for commonly used numbers

--- PokemonRandomizer/PokemonRandomizer/Backend/DataStructures/Trainers/IHasTrainerPokemonEvs.cs
+++ PokemonRandomizer/PokemonRandomizer/Backend/DataStructures/Trainers/IHasTrainerPokemonEvs.cs
@@ -8,6 +8,39 @@ namespace PokemonRandomizer.Backend.DataStructures.Trainers
 {
     public interface IHasTrainerPokemonEvs
     {
+        public const byte maxUsefulEvValue = 252;
+        public const int maxEvs = 510;
+        public const int leftoverEvs = maxEvs - (maxUsefulEvValue * 2);
         public byte[] EVs { get; set; }
+        public byte HpEVs
+        {
+            get => EVs[PokemonBaseStats.hpStatIndex];
+            set => EVs[PokemonBaseStats.hpStatIndex] = value;
+        }
+        public byte AttackEVs
+        {
+            get => EVs[PokemonBaseStats.atkStatIndex];
+            set => EVs[PokemonBaseStats.atkStatIndex] = value;
+        }
+        public byte DefenseEVs
+        {
+            get => EVs[PokemonBaseStats.defStatIndex];
+            set => EVs[PokemonBaseStats.defStatIndex] = value;
+        }
+        public byte SpeedEVs
+        {
+            get => EVs[PokemonBaseStats.spdStatIndex];
+            set => EVs[PokemonBaseStats.spdStatIndex] = value;
+        }
+        public byte SpAttackEVs
+        {
+            get => EVs[PokemonBaseStats.spAtkStatIndex];
+            set => EVs[PokemonBaseStats.spAtkStatIndex] = value;
+        }
+        public byte SpDefenseEVs
+        {
+            get => EVs[PokemonBaseStats.spDefStatIndex];
+            set => EVs[PokemonBaseStats.spDefStatIndex] = value;
+        }
     }
 }
