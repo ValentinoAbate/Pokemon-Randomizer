035e5dd4d29c06001da68aa8de05bdf6748f0802
Valentino Abate <valentinoabate1@gmail.com>
2025-10-31 00:14:05 -0700

    Overlay searcher helper function

--- PokemonRandomizer/PokemonRandomizer/Backend/RomHandling/Parsing/DSRomParser.cs
+++ PokemonRandomizer/PokemonRandomizer/Backend/RomHandling/Parsing/DSRomParser.cs
@@ -1,6 +1,27 @@
-﻿namespace PokemonRandomizer.Backend.RomHandling.Parsing
+﻿using PokemonRandomizer.Backend.DataStructures;
+using PokemonRandomizer.Backend.DataStructures.DS;
+using PokemonRandomizer.Search;
+
+namespace PokemonRandomizer.Backend.RomHandling.Parsing
 {
     public abstract class DSRomParser : RomParser
     {
+        // Search for a specific byte sequence in all overlays
+        protected static bool TryFindSequenceInOverlays(byte[] sequence, Rom rom, DSFileSystemData dsFileSystem, out int overlayId, out int offset)
+        {
+            for (int i = 0; i < dsFileSystem.Arm9Overlays.Count; ++i)
+            {
+                var overlay = dsFileSystem.GetArm9OverlayData(rom, i, out int startOffset, out int length);
+                int sequenceOffset = Kmp.Search(overlay.File, sequence, startOffset, startOffset + length);
+                if (sequenceOffset != -1)
+                {
+                    overlayId = i;
+                    offset = sequenceOffset - startOffset;
+                    return true;
+                }
+            }
+            overlayId = offset = -1;
+            return false;
+        }
     }
 }
