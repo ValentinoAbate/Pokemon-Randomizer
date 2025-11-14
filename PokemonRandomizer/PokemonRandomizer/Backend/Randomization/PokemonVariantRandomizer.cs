8314f4c7fa63162eef867d71819fe216b4a05d3f
Valentino Abate <valentinoabate1@gmail.com>
2025-11-13 23:31:44 -0800

    Revert accidentally added code

--- PokemonRandomizer/PokemonRandomizer/Backend/Randomization/PokemonVariantRandomizer.cs
+++ PokemonRandomizer/PokemonRandomizer/Backend/Randomization/PokemonVariantRandomizer.cs
@@ -1559,9 +1559,6 @@ namespace PokemonRandomizer.Backend.Randomization
 
         private void ModifyPalette(PokemonBaseStats pokemon, Settings settings, VariantData data)
         {
-            return;
-            if (data.TransformationType == TypeTransformation.None || pokemon.palette == null)
-                return;
             var paletteData = GetPaletteData(pokemon.species);
             paletteModifier.ModifyPalette(pokemon.palette, paletteData, data.VariantTypes);
             // Special castform handling.
